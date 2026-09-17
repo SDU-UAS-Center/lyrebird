ce924277660698cb4a482f6752498af6ffed37de
Ale Jarabo <alejp@mmmi.sdu.dk>
2026-09-17 14:05:20 +0200

    Session: own aircraft connection generation

--- LyrebirdApp/lyrebird-app/src/main/java/com/lyrebird/rc/telemetry/ProcessTelemetryRuntimeRegistry.kt
+++ LyrebirdApp/lyrebird-app/src/main/java/com/lyrebird/rc/telemetry/ProcessTelemetryRuntimeRegistry.kt
@@ -1,15 +1,29 @@
 package com.lyrebird.rc.telemetry
 
 import android.content.Context
+import dji.sdk.keyvalue.key.FlightControllerKey
+import dji.v5.et.create
+import dji.v5.et.get
+import dji.v5.et.listen
+import dji.v5.manager.KeyManager
 
 internal object ProcessTelemetryRuntimeRegistry {
     private val aircraftTelemetry = V5AircraftTelemetrySource()
     private var deviceStatusSource: DeviceStatusSource? = null
+    private var connectionListenerStarted = false
 
     @Synchronized
     fun attach(context: Context) {
         if (deviceStatusSource == null) {
             deviceStatusSource = DeviceStatusSource(context.applicationContext)
+        }
+        if (!connectionListenerStarted) {
+            val connectionKey = FlightControllerKey.KeyConnection.create()
+            KeyManager.getInstance().listen(connectionKey, this) { _, connected ->
+                aircraftTelemetry.setConnectionState(connected == true)
+            }
+            aircraftTelemetry.setConnectionState(connectionKey.get(false))
+            connectionListenerStarted = true
         }
     }
 
